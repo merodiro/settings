5301989250bd85432811cc402eacab5a1420e0f7
Amr A.Mohammed <merodiro@gmail.com>
2018-01-23 04:40:50 +0200

    solve conflict

--- src/observers/SettingObserver.php
+++ src/observers/SettingObserver.php
@@ -7,10 +7,6 @@ use Illuminate\Support\Facades\Cache;
 
 class SettingObserver
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 742e658f3d6445afb3b5e5f5e9bf1e3f44b5e7bb
     public function updated(Setting $setting)
     {
         $cache_key = config('settings.cache_prefix') . $setting->key;
